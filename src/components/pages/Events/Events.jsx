e180886040a658a289968de9be165a78e69b93b3
chiruvishal <86530058+chiruvishal@users.noreply.github.com>
2023-05-15 21:18:02 +0530

    2 files added

---
+++ src/components/pages/Events/Events.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Events.css';
+export default function FacultyOverview() {
+  return (
+    <>
+      <h1 className="consulting">FacultyOverview</h1>
+    </>
+  );
+}
