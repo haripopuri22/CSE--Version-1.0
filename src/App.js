9557dc5e779bac15dae6a6d8a594c67e779916d5
chiruvishal <86530058+chiruvishal@users.noreply.github.com>
2023-05-15 20:56:51 +0530

    1 file modified, 2 added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import './App.css';
 import Home from './components/pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import FacultyOverview from './components/pages/FacultyOverview/FacultyOverview';
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
 import ContactUs from './components/pages/ContactUs';
@@ -18,7 +19,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/services" component={Services} />
-        <Route path="/products" component={Products} />
+        <Route path="/products" component={FacultyOverview} />
         <Route path="/contact-us" component={ContactUs} />
         <Route path="/sign-up" component={SignUp} />
         <Route path="/marketing" component={Marketing} />
