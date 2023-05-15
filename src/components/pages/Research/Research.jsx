e217a58ac1e98ff0a6dda9eff161167aa72ce264
chiruvishal <86530058+chiruvishal@users.noreply.github.com>
2023-05-15 21:14:56 +0530

    2 files added

---
+++ src/components/pages/Research/Research.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Research.css';
+export default function Research() {
+  return (
+    <>
+      <h1 className="consulting">FacultyOverview</h1>
+    </>
+  );
+}
