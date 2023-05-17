3d1116bd3d1049055c51b807c1206ade99770aa2
haripopuri22 <100458927+haripopuri22@users.noreply.github.com>
2023-05-17 11:50:27 +0530

    2 files modified

--- src/components/pages/CurricullumAndOverview/Curriculum.jsx
+++ src/components/pages/CurricullumAndOverview/Curriculum.jsx
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Curriculum.css';
+
 export default function Curriculum() {
+  const [selectedYear, setSelectedYear] = useState('1st Year');
+  const [selectedSemester, setSelectedSemester] = useState('Semester I');
+
+  const handleYearChange = (event) => {
+    setSelectedYear(event.target.value);
+  };
+
+  const handleSemesterChange = (event) => {
+    setSelectedSemester(event.target.value);
+  };
+
+  const handleShowButtonClick = () => {
+    alert(`You selected: ${selectedYear} - ${selectedSemester}`);
+  };
+
   return (
-    <>
-      <h1 className="consulting">Curriculum</h1>
-    </>
+    <div className="container">
+      <div className="box">
+        <div className="column">Curriculum</div>
+        <div className="dropdown-container">
+          <select
+            className="dropdown"
+            value={selectedYear}
+            onChange={handleYearChange}
+          >
+            <option value="1st Year">1st Year</option>
+            <option value="2nd Year">2nd Year</option>
+            <option value="3rd Year">3rd Year</option>
+            <option value="4th Year">4th Year</option>
+          </select>
+          <div className="gap"></div>
+          <select
+            className="dropdown"
+            value={selectedSemester}
+            onChange={handleSemesterChange}
+          >
+            <option value="Semester I">Semester I</option>
+            <option value="Semester II">Semester II</option>
+          </select>
+        </div>
+        <br />
+        <button className="button" onClick={handleShowButtonClick}>
+          Show
+        </button>
+      </div>
+    </div>
   );
 }
