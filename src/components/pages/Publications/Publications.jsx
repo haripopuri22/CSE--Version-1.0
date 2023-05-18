6c95a6bead737501f2f47d5d62ae61de3019e6f4
chiruvishal <86530058+chiruvishal@users.noreply.github.com>
2023-05-18 17:37:36 +0530

    24 files added

---
+++ src/components/pages/Publications/Publications.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Publications.css';
+export default function Publications() {
+  return (
+    <>
+      <h1 className="consulting">Publications</h1>
+    </>
+  );
+}
